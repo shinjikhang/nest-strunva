e3bf68b13667e5aaf6e297bb6fcdf5e116c8b3d2
Shinji Khang <huukhang1999@gmail.com>
2023-07-13 14:29:16 +0700

    add package socket io

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -19,7 +19,6 @@ export class AuthGuard implements CanActivate {
       context.getClass(),
     ]);
     if (isPublic) {
-      // 💡 See this condition
       return true;
     }
 
@@ -32,8 +31,6 @@ export class AuthGuard implements CanActivate {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: '' + process.env.JWT_SECRET_KEY,
       });
-      // 💡 We're assigning the payload to the request object here
-      // so that we can access it in our route handlers
       request['user'] = payload;
     } catch {
       throw new UnauthorizedException();
